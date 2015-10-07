65e15482d322272d086c8eef85dd18ce39d8878e
Iain Nash <iain@codejoust.com>
2015-10-06 20:36:15 -0700

    Initial Commit

---
+++ BackgroundEstimator/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  BackgroundEstimator
+//
+//  Created by Iain Nash on 10/6/15.
+//  Copyright © 2015 Iain Nash. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+    return 0;
+}
